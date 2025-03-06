f7a6af07f60baa9ef945030f6f91e9a4d137980e
Swosti <swostisambityadav@gmail.com>
2025-03-06 12:43:34 +0530

    Initial Commit

---
+++ HBDocScanner/HBDocScanner.h
@@ -0,0 +1,18 @@
+//
+//  HBDocScanner.h
+//  HBDocScanner
+//
+//  Created by hb on 06/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HBDocScanner.
+FOUNDATION_EXPORT double HBDocScannerVersionNumber;
+
+//! Project version string for HBDocScanner.
+FOUNDATION_EXPORT const unsigned char HBDocScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HBDocScanner/PublicHeader.h>
+
+
